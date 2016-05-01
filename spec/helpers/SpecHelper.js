d382ee34ceed582680110c575be2ef442df7b2fe
kcopper8 <kcopper8@gmail.com>
2016-05-01 15:38:48 +0900

    dev env

---
+++ spec/helpers/SpecHelper.js
@@ -0,0 +1,18 @@
+'use strict';
+
+beforeEach(function() {
+
+  jasmine.addMatchers({
+    toBeFunction: function() {
+      return {
+        compare: function(actual) {
+          return {
+            pass : typeof actual === 'function'
+          }
+        }
+      };
+    }
+  });
+
+
+});
